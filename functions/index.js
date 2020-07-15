17f9f99667e3af042fccc9c94383efafccf5c15a
Joe RN <joseph.allen.1994@hotmail.com>
2020-07-15 20:08:11 +0100

    enable CORS

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,9 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 const express = require("express");
+const cors = require("cors")({ origin: true });
 const app = express();
+app.use(cors());
 const firebase = require("firebase");
 
 admin.initializeApp();
