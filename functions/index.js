c784b74a440176a0f94756805dc9669f423a2ccf
Joe RN <joseph.allen.1994@hotmail.com>
2020-07-15 12:16:14 +0100

    Add signup route

--- functions/index.js
+++ functions/index.js
@@ -24,52 +24,52 @@ app.get("/helloWorld", (request, response) => {
   response.send("Hello from Firebase!");
 });
 
-// app.post("/signup", (req, res) => {
-//   const newUser = {
-//     email: req.body.email,
-//     password: req.body.password,
-//     confirmPassword: req.body.confirmPassword,
-//     handle: req.body.handle,
-//   };
+app.post("/signup", (req, res) => {
+  const newUser = {
+    email: req.body.email,
+    password: req.body.password,
+    confirmPassword: req.body.confirmPassword,
+    handle: req.body.handle,
+  };
 
-//   //   TODO: validate data
-//   let token, userId;
-//   db.doc(`/users/${newUser.handle}`)
-//     .get()
-//     .then((doc) => {
-//       if (doc.exists) {
-//         return res.status(400).json({ handle: "this handle is already taken" });
-//       } else {
-//         return firebase
-//           .auth()
-//           .createUserWithEmailAndPassword(newUser.email, newUser.password);
-//       }
-//     })
-//     .then((data) => {
-//       userId = data.user.uid;
-//       return data.user.getIdToken();
-//     })
-//     .then((idToken) => {
-//       token = idToken;
-//       const userCredentials = {
-//         handle: newUser.handle,
-//         email: newUser.email,
-//         createdAt: new Date().toISOString(),
-//         userId,
-//       };
-//       return db.doc(`/users/${newUser.handle}`).set(userCredentials);
-//     })
-//     .then(() => {
-//       return res.status(201).json({ token });
-//     })
-//     .catch((err) => {
-//       console.error(err);
-//       if (err.code === "auth/email-already-in-use") {
-//         return res.status(400).json({ email: "Email is already in use" });
-//       } else {
-//         return res.status(500).json({ error: err.code });
-//       }
-//     });
-// });
+  //   TODO: validate data
+  let token, userId;
+  db.doc(`/users/${newUser.handle}`)
+    .get()
+    .then((doc) => {
+      if (doc.exists) {
+        return res.status(400).json({ handle: "this handle is already taken" });
+      } else {
+        return firebase
+          .auth()
+          .createUserWithEmailAndPassword(newUser.email, newUser.password);
+      }
+    })
+    .then((data) => {
+      userId = data.user.uid;
+      return data.user.getIdToken();
+    })
+    .then((idToken) => {
+      token = idToken;
+      const userCredentials = {
+        handle: newUser.handle,
+        email: newUser.email,
+        createdAt: new Date().toISOString(),
+        userId,
+      };
+      return db.doc(`/users/${newUser.handle}`).set(userCredentials);
+    })
+    .then(() => {
+      return res.status(201).json({ token });
+    })
+    .catch((err) => {
+      console.error(err);
+      if (err.code === "auth/email-already-in-use") {
+        return res.status(400).json({ email: "Email is already in use" });
+      } else {
+        return res.status(500).json({ error: err.code });
+      }
+    });
+});
 
 exports.api = functions.region("europe-west1").https.onRequest(app);
